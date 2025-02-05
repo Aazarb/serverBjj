88bfbcf2f2e72c8bc33f41147105523acb32e750
Aazarb <zaraba.adil@hotmail.fr>
2025-02-05 23:59:39 +0100

    AuthController updated
    - confirmRegistration endpoint added

--- src/main/java/com/example/backend/controllers/AuthController.java
+++ src/main/java/com/example/backend/controllers/AuthController.java
@@ -4,10 +4,7 @@ import com.example.backend.dto.UserDto;
 import com.example.backend.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,5 +21,11 @@ public class AuthController {
     public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto) {
         UserDto userToRegisterDto = new UserDto(userDto.getUsername(),userDto.getEmail(),userDto.getPassword());
         return new ResponseEntity<>(userService.register(userToRegisterDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/confirm")
+    public ResponseEntity<Void> confirmRegistration(@RequestParam String token) {
+        userService.confirmRegistration(token);
+        return  ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
